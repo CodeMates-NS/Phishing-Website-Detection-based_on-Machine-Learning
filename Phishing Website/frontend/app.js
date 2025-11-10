5085a7243205ef77eeb6403326f2c7f87ab15c5a
Nabeel UrRehman <nabeelrehman1276@gmail.com>
2025-11-11 01:22:36 +0530

    Update app.js

--- Phishing Website/frontend/app.js
+++ Phishing Website/frontend/app.js
@@ -1,5 +1,5 @@
 // =============== CONFIGURATION ===============
-const backendURL = "https://phishing-website-detection-5hn3.onrender.com/predict";
+const backendURL = "https://phishing-website-detection-based-on.onrender.com";
 // Change this only if you redeploy your backend on a different Render URL
 // =============================================
 
